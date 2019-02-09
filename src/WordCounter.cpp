e613d763d24f15604247f5360d3568fbe13a8c43
monnier <monnier.jacques.lgm@gmail.com>
2019-02-09 11:48:12 +0100

    Test 3/4 passed
    Test number of space doesn't count the last word

--- src/WordCounter.cpp
+++ src/WordCounter.cpp
@@ -1,23 +1,40 @@
 #include "../inc/WordCounter.hpp"
 
+//ispunct
+
 WordCounter::WordCounter(const string textLine){
 	this->textLine = textLine;
 }
 
 unsigned int WordCounter::count(void){
 	unsigned int	nbWord 			=	0;
-	bool			lockCountLast	=	false;
-	
+	bool			lockCount		=	false;
+
 	string tmpTextLine	=	this->textLine;
 
-	for(unsigned int i = 0 ; i < tmpTextLine.size() ; i++){
-		if ((!isalpha(tmpTextLine[i])) && (!isdigit(tmpTextLine[i]))){
-			nbWord +=	1;
-		}
-		//To count the last word
-		if(tmpTextLine[i+1] == '\0'){
+	for(unsigned int i = 0 ; i <= tmpTextLine.size() ; i++){
+		if (isblank(tmpTextLine[i]) 
+			&& !ispunct(tmpTextLine[i])
+			&& !isdigit(tmpTextLine[i])){
 			nbWord +=	1;
 		}
 	}
 	return nbWord;
 }
+
+/* work without spaces
+for(unsigned int i = 0 ; i <= tmpTextLine.size() ; i++){
+		if ((!isalpha(tmpTextLine[i])) && (!isdigit(tmpTextLine[i])) && lockCount == false ){
+			nbWord +=	1;
+			lockCount = false;
+		}else{
+			lockCount = true;
+		}
+
+	}
+*/
+
+/*
+&& !isdigit(tmpTextLine[i]) 
+			&& !ispunct(tmpTextLine[i])
+*/
